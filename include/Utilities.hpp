b67289a760d9412148a39a17d6794b1b5605a3a9
Will Thomas <technimanx@googlemail.com>
2018-05-21 22:54:28 +0100

    Added math utilities

---
+++ include/Utilities.hpp
@@ -0,0 +1,8 @@
+#ifndef _UTILITIES_HPP_
+#define _UTILITIES_HPP_
+
+int Max(int a, int b);
+
+int Min(int a, int b);
+
+#endif // _UTILITIES_HPP_
---
+++ src/Utilities.cpp
@@ -0,0 +1,15 @@
+#include "../include/Utilities.hpp"
+
+int Max(int a, int b) {
+    if (a > b) {
+        return a;
+    }
+    return b;
+}
+
+int Min(int a, int b) {
+    if (a < b) {
+        return a;
+    }
+    return b;
+}
